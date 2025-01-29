a601220d7bf1f27505a79d4d4b925617ddc9436f
yujinGwon <gwonyj36@gmail.com>
2025-01-29 12:18:30 +0900

    프로필 및 게시글 상세 컴포넌트 작업

---
+++ src/components/PostDetail.tsx
@@ -0,0 +1,25 @@
+export default function PostDetail() {
+  return (
+    <>
+      <div className="post__detail">
+        <div className="post__box">
+          <div className="post__title">
+            포스트 타이틀 입니다.
+          </div>
+          <div className="post__profile-box">
+            <div className="post__profile" />
+            <div className="post__author-name">패스트캠퍼스</div>
+            <div className="post__date">2023.07.08 토요일</div>
+          </div>
+          <div className="post__utils-box">
+            <div className="post__delete">삭제</div>
+            <div className="post__edit">수정</div>
+          </div>
+          <div className="post__text">
+            abcdefghijklmnopqrstuvwxyz
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,45 +1,13 @@
-import { Link } from "react-router-dom";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import PostList from "../../components/PostList";
 
 export default function Home() {
   return (
-    <div>
-      <header>
-        <div>
-          <Link to="/posts/new">글쓰기</Link>
-          <Link to="/posts">게시글</Link>
-          <Link to="/profile">프로필</Link>
-        </div>
-      </header>
-      <div className="post__navigation">
-        <div className="post__navigation--active">전체</div>
-        <div>나의 글</div>
-      </div>
-      <div className="post__list">
-        {[...Array(10)].map((e, index) => (
-          <div key={index} className="post__box">
-            <Link to={`/posts/${index}`}>
-              <div className="post__profile-box">
-                <div className="post__profile" />
-                <div className="post__author-name">패스트캠퍼스</div>
-                <div className="post__date">2023.07.08 토요일</div>
-              </div>
-              <div className="post__title">게시글 {index}</div>
-              <div className="post__text">
-                abcdefghijklmnopqrstuvwxyz
-              </div>
-              <div className="post__utils-box">
-                <div className="post__delete">삭제</div>
-                <div className="post__edit">수정</div>
-              </div>
-            </Link>
-          </div>
-        ))}
-      </div>
-      <footer>
-        <Link to="/posts/new">글쓰기</Link>
-        <Link to="/posts">게시글</Link>
-        <Link to="/profile">프로필</Link>
-      </footer>
-    </div>
+    <>
+      <Header />
+      <PostList />
+      <Footer />
+    </>
   );
 }
